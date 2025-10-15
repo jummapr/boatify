ce72a0731c51461c6766b254e8e4fd10635f1390
jumma <aarifhingorja4@gmail.com>
2025-10-15 13:36:39 +0530

    setup the turborepo

--- apps/widget/app/page.tsx
+++ apps/widget/app/page.tsx
@@ -1,13 +1,13 @@
 import { Button } from "@workspace/ui/components/button"
-import {add} from "@workspace/math/add"
+import { Input } from "@workspace/ui/components/input"
 
 export default function Page() {
   return (
     <div className="flex items-center justify-center min-h-svh">
       <div className="flex flex-col items-center justify-center gap-4">
         <h1 className="text-2xl font-bold">Hello World</h1>
-        <p className="text-lg">2 + 3 = {add(2, 3)}</p>
         <Button size="sm">Button</Button>
+        <Input placeholder="Input" />
       </div>
     </div>
   )
